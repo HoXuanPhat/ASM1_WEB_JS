2ffde7ff70dfedd49f2618e1093ce1d83f515514
ASM1_WEB_JS <hxphat2004@gmail.com>
2024-10-24 21:41:40 +0700

    init project

---
+++ ts/data.ts
@@ -0,0 +1,32 @@
+type THocVien = { id:number; ten:string; phone:string; phai:TGioiTinh}
+type TGioiTinh = string | boolean;
+type TKhoaHoc = { id:number; tenkhoa: string; khaigiang: string; ghichu:string  }
+type TDangHoc = THocVien & TKhoaHoc; 
+type TCoSo = { id:number;  ten:string;  diachi:string;  hinh:string; }
+const khoahoc_arr:TKhoaHoc[] = [
+    {id:1, tenkhoa:"18", khaigiang:"2018", "ghichu":"Nhiều sinh viên"},
+    {id:2, tenkhoa:"19", khaigiang:"2019", "ghichu":"Mùa Covid học online"},
+    {id:3, tenkhoa:"20", khaigiang:"2020", "ghichu":"Sinh viên đông"},
+    {id:4, tenkhoa:"21", khaigiang:"2021", "ghichu":"Chương trình mới"},
+    {id:5, tenkhoa:"22", khaigiang:"2022", "ghichu":"Chương trình mới"},
+    {id:6, tenkhoa:"23", khaigiang:"2023", "ghichu":" Học 6 kỳ"},
+ ];
+let hv_arr:TDangHoc[] = [
+    {id:1, ten:"Ngọc Khuê", phone:"066336633", phai:"Nữ", tenkhoa:"18", khaigiang:"2018", ghichu:"dễ thương"},
+    {id:2, ten:"Hoàng Dung", phone:"075569922", phai:true, tenkhoa:"19", khaigiang:"2019", ghichu:"dễ mến"},
+    {id:3, ten:"Mỹ Duyên", phone:"035353535", phai:"Nam", tenkhoa:"20", khaigiang:"2020", ghichu:"học giỏi"},
+    {id:4, ten:"Xuân Phát", phone:"014756565", phai:false, tenkhoa:"21", khaigiang:"2021", ghichu:"hát hay"},
+    {id:5, ten:"Ngọc Hoàng", phone:"0363636365", phai:"Nam", tenkhoa:"22", khaigiang:"2022", ghichu:"nhanh nhẹn"},
+    {id:6, ten:"Đức Tâm", phone:"077556667", phai:"Nam", tenkhoa:"23", khaigiang:"2023", ghichu:"khỏe khắn"} 
+];
+const coso_arr:TCoSo[] = [
+    { id:1, ten:"Cơ sở Hồ Chí Minh", diachi: "Tòa nhà QTSC9, Tô Ký, phường Tân Chánh Hiệp, Q12, TP HCM", "hinh": "images/hcm.jpeg"},
+    { id:2, ten:"Cơ sở Hà Nội", diachi: "13 phố Trịnh Văn Bô, phường Phương Canh, quận Nam Từ Liêm, TP Hà Nội", "hinh": "images/hn.jpg"},
+    { id:3, ten:"Cơ sở Đà Nẵng", diachi: "219 Nguyễn Sinh Sắc, phường Hoà Minh, quận Liên Chiểu, TP Đà Nẵng", "hinh": "images/dn.webp"},
+    { id:4, ten:"Cơ sở Cần Thơ", diachi: "Đường số 22, phường Thường Thạnh, quận Cái Răng, TP Cần Thơ", "hinh": "images/ct.jpg"},
+    { id:5, ten:"Cơ sở Hải Phòng", diachi: "271 Lê Thánh Tông, phường Máy Chai, q.Ngô Quyền, TP Hải Phòng", "hinh": "images/hp.jpg"},
+    { id:6, ten:"Cơ sở Thái Nguyên", diachi: "Đê Mỏ Bạch, phường Quyết Thắng, TP Thái Nguyên", "hinh": "images/tn.jpg"}
+];
+export { TCoSo, TDangHoc, THocVien, TKhoaHoc, TGioiTinh}
+export { hv_arr, coso_arr, khoahoc_arr}
+  
